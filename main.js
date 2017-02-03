7e85c66b8f4ed3151e8c591033175948576de1cd
Pg Biel <pgbiel@hotmail.com.br>
2017-02-03 17:20:42 -0200

    My bad

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 (function(){
   isPrime=function(n, o){var r=2;if(o&&n==1||n==-1)r=!1;if(n>-1>>>0)throw new RangeError("Number must be under "+(-1>>>0));var g=2;do{if(n%g==0)r=n==2;g++;}while(g<=Math.pow(n, .5));return r==2?!0:r;};
-  if (window) window.isPrime = isPrime;
-  else if (module) {
-    module.exports = {
-      isPrime: isPrime
-    };
-  }
+  try {
+    if (window) window.isPrime = isPrime;
+    return;
+  } catch (window_does_not_exist_bcuz_nodejs) {} //eslint-disable-line no-empty
+  try {
+    if (module) {
+      module.exports = {
+        isPrime: isPrime
+      };
+    }
+  } catch (how_did_this_happen) {} //eslint-disable-line no-empty
+  return;
 })();
