4d663e4775075d587254cc5542d41284c0e96d84
Pg Biel <pgbiel@hotmail.com.br>
2017-02-03 18:43:47 -0200

    uh?

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 (function(){
-  isPrime=function(n, o){var r=2;if(!o&&n==1||n==-1)r=!1;if(n>-1>>>0)throw new RangeError("Number must be under "+(-1>>>0));var g=2;do{if(n%g==0)r=n==2;g++;}while(g<=Math.pow(n, .5));return r==2?!0:r;};
+  isPrime=function(n, o){var r=2;if((!o)&&n==1||n==-1)r=!1;if(n>-1>>>0)throw new RangeError("Number must be under "+(-1>>>0));var g=2;do{if(n%g==0)r=n==2;g++;}while(g<=Math.pow(n, .5));return r==2?!0:r;};
   try {
     if (window) window.isPrime = isPrime;
     return;
